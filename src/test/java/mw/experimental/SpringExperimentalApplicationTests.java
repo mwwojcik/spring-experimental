1508dc45b48b04c67ca0ac3bd5bf1380708eb630
mwwojcik <mw.wojcik@gmail.com>
2020-06-03 07:09:31 +0200

    init

---
+++ src/test/java/mw/experimental/SpringExperimentalApplicationTests.java
@@ -0,0 +1,13 @@
+package mw.experimental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExperimentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
